aa4cd0b041a53f5d3c6134199c133d5056e8d447
Hasanboy2003 <maxmudovhasanboy070@gmail.com>
2023-07-17 12:58:06 +0500

    Telegram bot registration

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,8 +33,10 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
-    implementation("org.telegram:telegrambots:6.7.0")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.telegram:telegrambots:6.1.0")
+
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/uz/zerone/supporttelegrambot/SupportTelegramBot.kt
+++ src/main/kotlin/uz/zerone/supporttelegrambot/SupportTelegramBot.kt
@@ -1,21 +1,22 @@
 package uz.zerone.supporttelegrambot
 
+import org.springframework.stereotype.Component
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
-import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.meta.api.objects.Update
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
 
+@Component
+class SupportTelegramBot: TelegramLongPollingBot() {
+    override fun getBotToken(): String = "6005965806:AAGx17eBrfH2z2DvIeYu2WZPe6d_BUfnJ4s"
 
-class SupportTelegramBot() : TelegramLongPollingBot(
-    "6005965806:AAGx17eBrfH2z2DvIeYu2WZPe6d_BUfnJ4s"
-) {
+    override fun getBotUsername() :String = " session_support_bot"
 
-    override fun getBotUsername(): String {
-        return "https://t.me/session_support_bot"
-    }
+    override fun onUpdateReceived(update: Update) {
+        val text = update.message.text
+        val chatId = update.message.chatId
+        println(text)
+        println(chatId)
+        println(update.message)
 
-    override fun onUpdateReceived(update: Update?) {
-        TODO("Not yet implemented")
     }
 }
 
